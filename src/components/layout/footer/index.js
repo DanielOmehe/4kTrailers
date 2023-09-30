83e2424680ccb19aba2b563ee32fcd624a4a976f
DaniOmehe <danielomehe66@gmail.com>
2023-09-30 21:02:19 +0100

    Build footer and discover section

---
+++ src/components/layout/footer/index.js
@@ -0,0 +1,29 @@
+import "./index.scss";
+import { Link } from "react-router-dom";
+
+const RustedRageFooter = () => {
+	return (
+		<footer className="rusted-rage-footer">
+			<div className="rusted-rage-footer-container">
+				<ul className="rusted-rage-footer-items">
+					<Link to={"/"}>Home</Link>
+					<Link to={"/"}>Movies</Link>
+					<Link to={"/"}>Tv Shows</Link>
+					<Link to={"/"}>Trending</Link>
+					<Link to={"/"}>Watchlist</Link>
+				</ul>
+				<p>
+					RustedRage - Your destination for free movie streaming online. Watch
+					movies online for free, anytime, anywhere. Explore our vast collection
+					and experience cinematic wonders at your fingertips.
+				</p>
+				<p>
+					Links: watch movies online, free watch anime online, free watch anime
+					online
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default RustedRageFooter;
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,10 +1,14 @@
-import RustedRageHeader from "./header"
-import RustedRageContent from "./content"
+import RustedRageHeader from "./header";
+import RustedRageContent from "./content";
+import RustedRageFooter from "./footer";
+import RustedRageDiscover from "./content/discover";
 
 const RustedRageHomepage =()=>(
     <>
         <RustedRageHeader />
         <RustedRageContent />
+        <RustedRageDiscover />
+        <RustedRageFooter />
     </>
 )
 
