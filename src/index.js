bd39ae422e3932f8c2c5f14451864c77b89ac2b2
Daniel Omehe <103520487+DanielOmehe@users.noreply.github.com>
2023-10-11 11:40:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ const root = createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<RustedRageProvider>
-        <Toaster position="top-center" />
+        		<Toaster position="top-center" />
 			<RouterProvider router={router} />
 		</RustedRageProvider>
 	</React.StrictMode>
